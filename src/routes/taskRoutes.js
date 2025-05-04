e07b0c2da768d71c8a231319e656ec186187149c
andre <andregabschuh@gmail.com>
2025-05-04 13:39:53 -0300

    Mudança de rotas

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const taskController = require("../controllers/taskController");
 const authMidlleware = require("../middleware/authMiddleware");
 
-router.post("/task", authMidlleware, taskController.createTask);
-router.get("/task", authMidlleware, taskController.getTasks);
-router.delete("/task/:id", authMidlleware, taskController.deleteTaskById);
-router.put("/task/:id", authMidlleware, taskController.updateTaskStatusById);
+router.post("", authMidlleware, taskController.createTask);
+router.get("", authMidlleware, taskController.getTasks);
+router.delete("/:id", authMidlleware, taskController.deleteTaskById);
+router.put("/:id", authMidlleware, taskController.updateTaskStatusById);
 
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 require("./db")
 
 const express = require('express');
-const cors = require("cors");
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require("./routes/taskRoutes");
 const authMidlleware = require("./middleware/authMiddleware");
@@ -10,21 +9,15 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
 app.use(express.json());
-app.use(express.static('models'));
-app.use("/api", taskRoutes);
+app.use(express.static("public"));
 
 app.use('/api/auth', authRoutes);
-
-app.use(express.static("public"));
+app.use("/api/task", authMidlleware, taskRoutes);
 
 app.get('/', (req, res) => {
   res.send('Servidor rodando com Express!');
 });
-
-app.use("/api/tasks", authMidlleware, taskRoutes);
-
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
